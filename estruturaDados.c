eb642269b61afa585b6aa6629cf699f27df5f267
MathRogelin <matheusrogelin15@gmail.com>
2025-08-30 11:45:48 -0300

    Novos projetos

--- estruturaDados.c
+++ estruturaDados.c
@@ -1,24 +1,20 @@
-#include <stdio.h>   // para printf
-#include <stdint.h>  // se quiser usar tipos como int32_t etc.
+#include <stdio.h>
 
-// Definição da struct fora do main
-struct objeto {
-    int altura, largura;
-    char nome[50];
-    struct objeto* objeto2; // pode apontar para outro objeto
-};
+// se declara fora um struct, não dentro da função principal (main)
+typedef struct{
+    char Nome[50];
+    int Idade;
+    float Nota;
+} Aluno;
 
-int main() {
-    // Criando um objeto
-    struct objeto obj1 = { 180, 75, "Caixa", NULL };
-
-    // Criando outro objeto e ligando
-    struct objeto obj2 = { 200, 100, "Armario", NULL };
-    obj1.objeto2 = &obj2; // obj1 -> obj2
-
-    // Imprimindo
-    printf("Objeto 1: %s (%dx%d)\n", obj1.nome, obj1.altura, obj1.largura);
-    printf("Objeto 2: %s (%dx%d)\n", obj1.objeto2->nome, obj1.objeto2->altura, obj1.objeto2->largura);
+// inicia o programa
+int main(){
+    // cria uma cópia e recebe valores (em ordem)
+    Aluno aluno1 = {"Joao", 16, 9.5};
+    // mensagem
+    printf("Nome: %s\n", aluno1.Nome);
+    printf("Idade: %d\n", aluno1.Idade);
+    printf("Nota: %.1f\n", aluno1.Nota);
 
     return 0;
 }
